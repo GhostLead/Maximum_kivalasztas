78f3a8466e6ede6f4359235ba34c62b00267823d
DevDominik <71924117+DevDominik@users.noreply.github.com>
2022-09-08 13:37:21 +0200

    Update Maximum_kivalasztas.py

--- Maximum_kivalasztas.py
+++ Maximum_kivalasztas.py
@@ -1,22 +1,31 @@
-
 '''
-
     Stiegelmayer Bálint
     Barizs Márton Dániel
     Kecskés Dominik Bálint
-
 '''
+
+import random
 
 szamok = []
 
-for elem in range(1, 101):
-    szamok.append(elem)
+for elem in range(1, 11):
+    szamok.append(random.randint(1, 100))
 
+print(szamok)
 
-legnagyobb_szam = szamok[0]
+for elem_index in range(len(szamok)-1, 0, -1):
+    max_index = 0
+    csere1 = szamok[elem_index]
+    csere2 = 0
+    for keresett in range(0, elem_index):
+        if szamok[max_index] < szamok[keresett]:
+            max_index = keresett
+            csere2 = szamok[max_index]
+    if csere2 > 0:
+        szamok.pop(max_index)
+        szamok.insert(max_index, csere1)
+        szamok.pop(elem_index)
+        szamok.insert(elem_index, csere2)
+        print(szamok)
 
-for elem in szamok:
-    if elem > legnagyobb_szam:
-        legnagyobb_szam = elem
-
-print("A listában talált legnagyobb szám a", legnagyobb_szam)
+print(szamok)
